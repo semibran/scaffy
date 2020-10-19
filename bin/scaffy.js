227970949404df62cd4e72f781a3410764654759
Brandon Semilla <semibran@gmail.com>
2020-10-18 18:47:32 -0700

    keyword order and sorting, remove direct data passage

--- bin/scaffy.js
+++ bin/scaffy.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var scaffy = require("../lib/scaffy")
 var scan = require("../lib/scan")
+var priority = require("./priority.json")
 var pkg = require("../package.json")
 var fs = require("fs")
 var path = require("path")
@@ -9,7 +10,6 @@ var parse = require("minimist")
 var cwd = process.cwd()
 var args = process.argv.slice(2)
 var argv = parse(args, {
-	"--": true,
 	boolean: [ "help", "version" ],
 	alias: {
 		h: "help",
@@ -37,14 +37,7 @@ if (argv.version) {
 	process.exit()
 }
 
-var data = argv["--"]
-if (!data.length) {
-	data = {}
-} else {
-	data = parse(data, { string: [ "--", "_" ] })
-	delete data._
-}
-
+var data = {}
 var src = argv.input || argv._[0] || process.cwd()
 var opts = {
 	data: data,
@@ -58,10 +51,6 @@ console.log("using template `" + path.basename(src) + "`")
 
 scan(src, opts, function (err, keywords) {
 	if (err) throw err
-	for (var keyword in data) {
-		var value = data[keyword]
-		console.log("- " + keyword + ": " + value)
-	}
 	if (keywords.length) {
 		for (var i = 0; i < keywords.length; i++) {
 			var keyword = keywords[i]
@@ -69,7 +58,18 @@ scan(src, opts, function (err, keywords) {
 				keywords.splice(i--, 1)
 			}
 		}
-		ask(keywords, data, write)
+
+		keywords.sort(function (a, b) {
+			return findpriority(a) - findpriority(b)
+		})
+
+		function findpriority(keyword) {
+			var index = priority.indexOf(keyword)
+			return index >= 0 ? index : parseInt(keyword[0], 36) * keywords.length
+		}
+
+		var index = 0
+		ask(keywords, index, data, write)
 	} else {
 		write(data)
 	}
@@ -84,16 +84,23 @@ function write(data) {
 	})
 }
 
-function ask(keywords, data, cb) {
-	var keyword = keywords[0]
+function ask(keywords, index, data, cb) {
+	var keyword = keywords[index]
 	if (keyword) {
-		io.question("- " + keyword + ": ", function (value) {
+		index++
+		var prompt = format(keywords, keyword, index)
+		io.question(prompt, function (value) {
 			data[keyword] = value
-			keywords.shift()
-			ask(keywords, data, cb)
+			if (keywords[index]) {
+				ask(keywords, index, data, cb)
+			}
 		})
 	} else {
 		cb(data)
 		io.close()
 	}
+}
+
+function format(keywords, keyword, index, value) {
+	return "(" + index + "/" + keywords.length + ") " + keyword + ": " + (value || "")
 }
